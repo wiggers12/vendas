ebaa3ce6105fd45ba0402ccf183141754b0aef02
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-21 20:55:13 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("gigas-cache").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
