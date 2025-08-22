87497b12fc1dab36648a891b3d893bec8d36c510
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-21 21:37:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open("gigas-cache").then(cache => {
       return cache.addAll([
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        "index.html",
+        "manifest.json",
+        "IMG_6128.jpeg",
+        "IMG_6138.png",
+        "IMG_6139.png",
+        "IMG_6169.jpeg",
+        "IMG_6170.jpeg",
+        "IMG_6173.jpeg",
+        "IMG_6174.jpeg"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
