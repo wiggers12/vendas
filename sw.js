0afe072937193b59a2f25c0da3bc343af869de03
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-24 11:06:23 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("gigas-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "IMG_6128.jpeg",
-        "IMG_6138.png",
-        "IMG_6139.png",
-        "IMG_6169.jpeg",
-        "IMG_6170.jpeg",
-        "IMG_6173.jpeg",
-        "IMG_6174.jpeg"
-      ]);
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("dashcontrol-cache").then(cache=>{
+      return cache.addAll(["index.html","manifest.json","favicon.png"]);
     })
   );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=> response || fetch(e.request))
   );
 });
